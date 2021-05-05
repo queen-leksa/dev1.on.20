5ab0898fb10d61c574c9de5ddbad80c39e552583
Leksa <surnacheva@ithub.ru>
2021-05-05 15:13:30 +0300

    Отображение картинок на странице (lightbox)

---
+++ 2021.05.05/script.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+    
+    let content = document.createElement("div");
+    content.className = "miniatures";
+    document.body.appendChild(content);
+
+    /* БЫЛО РАНЬШЕ! */
+    // images.forEach(img => {
+    //     let card = document.createElement("div");
+    //     card.className = "mini";
+    //     card.setAttribute("data-src", img);
+    //     card.style.backgroundImage = `url(${img})`;
+    // });
+
+    let html = images.map(img => `<div class="mini" data-src=${img} style="background-image: url(${img})"></div>`);
+    console.log(html);
+
+    content.innerHTML = html.join("");
+
+});
